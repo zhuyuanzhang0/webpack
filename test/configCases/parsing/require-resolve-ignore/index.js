155df242e8ac6059ccf1a5881309c1063b3fe0a7
alexander-akait <sheo13666q@gmail.com>
2025-02-06 22:09:41 +0300

    test: more

--- test/configCases/parsing/require-resolve-ignore/index.js
+++ test/configCases/parsing/require-resolve-ignore/index.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 it("should be able to ignore require.resolve()", () => {
 	const source = fs.readFileSync(path.join(__dirname, "bundle1.js"), "utf-8");
-	expect(source).toMatch(`require.resolve(/* webpackIgnore: true */ "./non-exists")`);
-	expect(source).toMatch(`createRequire(import.meta.url).resolve(/* webpackIgnore: true */ "./non-exists")`);
-	expect(source).toMatch(`require.resolve(/* webpackIgnore: true */ "./non-exists")`);
+	expect(source).toMatch('require.resolve(/* webpackIgnore: true */ "./non-exists")');
+	expect(source).toMatch('createRequire(import.meta.url).resolve(/* webpackIgnore: true */ "./non-exists")');
+	expect(source).toMatch('require.resolve(/* webpackIgnore: true */ "./non-exists")');
+	expect(source).toMatch("module.exports = 1")
 });
--- test/configCases/parsing/require-resolve-ignore/other.js
+++ test/configCases/parsing/require-resolve-ignore/other.js
@@ -4,5 +4,6 @@ const resolve = require.resolve(/* webpackIgnore: true */ "./non-exists");
 const createRequireResolve1 = createRequire(import.meta.url).resolve(/* webpackIgnore: true */ "./non-exists");
 const require = createRequire(import.meta.url);
 const createRequireResolve2 = require.resolve(/* webpackIgnore: true */ "./non-exists");
+const resolved = require.resolve(/* webpackIgnore: false */ "./module");
 
-export { resolve, createRequireResolve1, createRequireResolve2 }
+export { resolve, createRequireResolve1, createRequireResolve2, resolved }
